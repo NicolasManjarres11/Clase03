24c7501ad4dfac256bc20c666352c0d1768f43af
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2024-11-06 23:53:36 -0500

    Actualizacion hastael ejercicio 18

--- Tutoria/Clase04/src/Exercise18.java
+++ Tutoria/Clase04/src/Exercise18.java
@@ -1,5 +1,39 @@
+
+import java.util.Random;
+
 public class Exercise18 {
+
     public static void main(String[] args) {
-        
+        Random random = new Random();
+
+        int positiveNumbers = 0;
+        int positiveCounter = 0;
+        int negativeNumber = 0;
+        int negativeCounter = 0;
+        int zeroCounter = 0;
+
+        for (int i = 0; i < 121; i++) {
+            int num = random.nextInt(-100, 100);
+            if (num > 0) {
+                positiveNumbers += num;
+                positiveCounter++;
+            } else if (num < 0) {
+                negativeNumber += num;
+                negativeCounter++;
+            } else {
+                zeroCounter++;
+            }
+
+            if (i == 120) {
+                System.out.println("Suma de positivos: " + positiveNumbers);
+                System.out.println("Media de positivos: " + (positiveNumbers / positiveCounter));
+                System.out.println("");
+                System.out.println("Suma de Negativos: " + negativeNumber);
+                System.out.println("Media de positivos: " + (negativeNumber / negativeCounter));
+                System.out.println("");
+                System.out.println("Cantidad de ceros: " + zeroCounter);
+            }
+        }
+
     }
 }
